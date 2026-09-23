5db665d83ad2489405291aabed5db385d777cadb
tondegamer246-byte <tondegamer246@gmail.com>
2026-09-23 19:02:59 +0530

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,45 @@
+// Export generated high-resolution rabbit images
+// STRICTLY Black & White rabbits or Pure White rabbits only.
+
+import heroRabbits from './hero_rabbits_1790135442339.jpg';
+import bwCatHero from './bw_cat_hero_1790135461207.jpg';
+import whiteCatHero from './white_cat_hero_1790135474804.jpg';
+import bulkSavingsImg from './bulk_savings_1790135489164.jpg';
+
+import bwRabbitA from './bw_rabbit_a_1790135506658.jpg';
+import bwRabbitB from './bw_rabbit_b_1790135541311.jpg';
+import bwRabbitC from './bw_rabbit_c_1790135571872.jpg';
+import bwRabbitD from './bw_rabbit_d_1790135609663.jpg';
+import bwRabbitE from './bw_rabbit_e_1790135644985.jpg';
+
+import whiteRabbitA from './white_rabbit_a_1790135526706.jpg';
+import whiteRabbitB from './white_rabbit_b_1790135556879.jpg';
+import whiteRabbitC from './white_rabbit_c_1790135596371.jpg';
+import whiteRabbitD from './white_rabbit_d_1790135624598.jpg';
+import whiteRabbitE from './white_rabbit_e_1790135664034.jpg';
+
+import dietGuideImg from './rabbit_healthy_diet_1790151288984.jpg';
+import habitatGuideImg from './rabbit_cozy_habitat_1790151305636.jpg';
+
+export const IMAGES = {
+  hero: heroRabbits,
+  bwCategory: bwCatHero,
+  whiteCategory: whiteCatHero,
+  bulkSavings: bulkSavingsImg,
+  dietGuide: dietGuideImg,
+  habitatGuide: habitatGuideImg,
+  
+  // Black & White rabbit photos
+  bwA: bwRabbitA,
+  bwB: bwRabbitB,
+  bwC: bwRabbitC,
+  bwD: bwRabbitD,
+  bwE: bwRabbitE,
+
+  // Pure White rabbit photos
+  whiteA: whiteRabbitA,
+  whiteB: whiteRabbitB,
+  whiteC: whiteRabbitC,
+  whiteD: whiteRabbitD,
+  whiteE: whiteRabbitE,
+};
